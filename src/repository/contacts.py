f40a4d98c341f54d1d78e2d4cdfac491e0a4bcdf
C-Hurtmann <constantine2903@gmail.com>
2023-06-18 18:33:40 +0300

    add func get contacts with birthday on next week

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -1,26 +1,52 @@
 from typing import List
 
+from datetime import datetime, timedelta
+from sqlalchemy import and_, extract
 from sqlalchemy.orm import Session
 
 from src.database.models import Contact
 from src.schemas import ContactModel
 
-
-async def get_contacts(skip: int, limit: int, db: Session) -> List[Contact]:
-    contacts = db.query(Contact).offset(skip).limit(limit).all()
+# GET
+async def get_contacts(skip: int,
+                       limit: int,
+                       db: Session,
+                       first_name: str = None,
+                       last_name: str = None,
+                       email: str = None) -> List[Contact]:
+    filters = []
+    if first_name:
+        filters.append(Contact.first_name == first_name)
+    if last_name:
+        filters.append(Contact.last_name == last_name)
+    if email:
+        filters.append(Contact.email == email)
+    contacts = db.query(Contact).filter(and_(*filters)).offset(skip).limit(limit).all()
     return contacts
+
+async def get_contacts_with_bithday_on_next_week(db: Session):
+    today = datetime.today().date()
+    day_after_one_week = today + timedelta(days=7)
+    contacts = db.query(Contact).all()
+    filtered_contacts = []
+    for contact in contacts:
+        birthday_date = contact.birthday.replace(year=today.year)
+        print(birthday_date, type(birthday_date))
+        if birthday_date > today and birthday_date <= day_after_one_week:
+            filtered_contacts.append(contact)
+    return filtered_contacts
 
 async def get_contact(contact_id: int, db: Session) -> Contact:
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     return contact
-
+# POST
 async def create_contact(body: ContactModel, db: Session) -> Contact:
     contact = Contact(**body.dict())
     db.add(contact)
     db.commit()
     db.refresh(contact)
     return  contact
-
+# PUT
 async def update_contact(contact_id: int, body: ContactModel, db: Session) -> Contact | None:
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if contact:
@@ -32,10 +58,11 @@ async def update_contact(contact_id: int, body: ContactModel, db: Session) -> Co
         db.commit()
         db.refresh(contact)
     return contact
-
+# DELETE
 async def delete_contact(contact_id: int, db: Session) -> Contact | None:
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if contact:
         db.delete(contact)
         db.commit()
     return contact
+
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -11,12 +11,22 @@ from src.repository import contacts as repo_contacts
 router = APIRouter(prefix='/contacts', tags=['contacts'])
 
 @router.get('/', response_model=List[ContactResponce])
-async def read_contacts(skip: int, limit: int, db: Session = Depends(get_db)):
-    result = await repo_contacts.get_contacts(skip, limit, db)
+async def get_contacts(skip: int,
+                        limit: int,
+                        db: Session = Depends(get_db),
+                        first_name: str = Query(None, description='filter by first name'),
+                        last_name: str = Query(None, description='filter by last name'),
+                        email: str = Query(None, description='filter by email')):
+    result = await repo_contacts.get_contacts(skip, limit, db, first_name, last_name, email)
+    return result
+
+@router.get('/bithday_on_next_week', response_model=List[ContactResponce])
+async def get_contacts_with_birthday_on_next_week(db: Session = Depends(get_db)):
+    result = await repo_contacts.get_contacts_with_bithday_on_next_week(db)    
     return result
 
 @router.get('/{contact_id}', response_model=ContactResponce)
-async def read_contact(contact_id: int, db: Session = Depends(get_db)):
+async def get_contact(contact_id: int, db: Session = Depends(get_db)):
     result = await repo_contacts.get_contact(contact_id, db)
     if not result:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Contact not found')
