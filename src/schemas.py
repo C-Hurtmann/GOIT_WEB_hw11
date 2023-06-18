8978017b5f710c987b32b759487bd64168494661
C-Hurtmann <constantine2903@gmail.com>
2023-06-18 19:20:48 +0300

    upgrade validators for phone and birthday

--- src/schemas.py
+++ src/schemas.py
@@ -1,14 +1,24 @@
 from datetime import date
 from typing import Optional
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, constr, validator
 
 
 class ContactModel(BaseModel):
     first_name: str = Field(max_length=100)
     last_name: str = Field(max_length=100)
     email: EmailStr
-    phone: Optional[str]
+    phone: constr(
+        regex="^\+380\d{9}$",
+        strict=True,
+        strip_whitespace=True)
     birthday: date
+    
+    @validator('birthday')
+    def validate_birthday(cls, value):
+        today = date.today()
+        if value > today:
+            raise ValueError('Birthday must be in past')
+        return value
 
 
 class ContactResponce(ContactModel):
