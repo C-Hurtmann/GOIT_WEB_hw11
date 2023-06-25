c34763f7952ca159bfe294f26db57fffd1f568c4
C-Hurtmann <constantine2903@gmail.com>
2023-06-25 14:55:38 +0300

    update contacts repo with authentication

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -1,19 +1,20 @@
-from typing import List
+from typing import List, Optional
 
 from datetime import datetime, timedelta
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
-from src.database.models import Contact
+from src.database.models import Contact, User
 from src.schemas import ContactModel
 
 # GET
 async def get_contacts(skip: int,
                        limit: int,
+                       user: User,
                        db: Session,
-                       first_name: str = None,
-                       last_name: str = None,
-                       email: str = None) -> List[Contact]:
+                       first_name: Optional[str] = None,
+                       last_name:Optional[str] = None,
+                       email: Optional[str] = None) -> List[Contact]:
     filters = []
     if first_name:
         filters.append(Contact.first_name == first_name)
@@ -21,13 +22,14 @@ async def get_contacts(skip: int,
         filters.append(Contact.last_name == last_name)
     if email:
         filters.append(Contact.email == email)
+    filters.append(Contact.user_id == user.id)
     contacts = db.query(Contact).filter(and_(*filters)).offset(skip).limit(limit).all()
     return contacts
 
-async def get_contacts_with_bithday_on_next_week(db: Session) -> List[Contact]:
+async def get_contacts_with_bithday_on_next_week(user: User, db: Session) -> List[Contact]:
     today = datetime.today().date()
     day_after_one_week = today + timedelta(days=7)
-    contacts = db.query(Contact).all()
+    contacts = db.query(Contact).filter(Contact.user_id == user.id).all()
     filtered_contacts = []
     for contact in contacts:
         birthday_date = contact.birthday.replace(year=today.year)
@@ -36,19 +38,29 @@ async def get_contacts_with_bithday_on_next_week(db: Session) -> List[Contact]:
             filtered_contacts.append(contact)
     return filtered_contacts
 
-async def get_contact(contact_id: int, db: Session) -> Contact:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def get_contact(contact_id: int, user: User, db: Session) -> Contact:
+    contact = db.query(Contact).filter(and_(
+        Contact.id == contact_id,
+        Contact.user_id == user.id
+        )).first()
+
     return contact
 # POST
-async def create_contact(body: ContactModel, db: Session) -> Contact:
-    contact = Contact(**body.dict())
+async def create_contact(body: ContactModel, user: User, db: Session) -> Contact:
+    contact_data = body.dict()
+    contact_data.update({'user': user})
+    contact = Contact(**contact_data)
     db.add(contact)
     db.commit()
     db.refresh(contact)
     return  contact
 # PUT
-async def update_contact(contact_id: int, body: ContactModel, db: Session) -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def update_contact(contact_id: int, body: ContactModel, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(and_(
+        Contact.id == contact_id,
+        Contact.user_id == user.id
+        )).first()
+
     if contact:
         contact.first_name = body.first_name
         contact.last_name = body.last_name
@@ -59,8 +71,12 @@ async def update_contact(contact_id: int, body: ContactModel, db: Session) -> Co
         db.refresh(contact)
     return contact
 # DELETE
-async def delete_contact(contact_id: int, db: Session) -> Contact | None:
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+async def delete_contact(contact_id: int, user: User, db: Session) -> Contact | None:
+    contact = db.query(Contact).filter(and_(
+        Contact.id == contact_id,
+        Contact.user_id == user.id
+        )).first()
+
     if contact:
         db.delete(contact)
         db.commit()
