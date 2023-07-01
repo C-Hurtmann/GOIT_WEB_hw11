0104a42e30b9a0bf3347ca264396e746043c8f38
C-Hurtmann <constantine2903@gmail.com>
2023-07-01 15:04:45 +0300

    delete unnessesary logging

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-import logging
-
 from fastapi import FastAPI
 import uvicorn
 from uvicorn.logging import DefaultFormatter
 from src.routes import contacts
 from src.routes import auth
 
-
-logger = logging.getLogger('uvicorn debug')
-logger.setLevel(logging.DEBUG)
-
-handler = logging.StreamHandler()
-handler.setFormatter(DefaultFormatter())
-logger.addHandler(handler)
 
 app = FastAPI()
 
@@ -21,7 +12,6 @@ app.include_router(contacts.router, prefix='/api')
 
 @app.get('/')
 def main():
-    logger.debug('hello')
     return {'message': 'Welcome'}
 
 
