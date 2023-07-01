c0e042208268183b46d1100b38be2238d7cdf414
C-Hurtmann <constantine2903@gmail.com>
2023-07-01 22:20:24 +0300

    test request limiter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from redis.asyncio import Redis
 from fastapi import FastAPI
+from fastapi_limiter import FastAPILimiter
 import uvicorn
 
 from src.routes import contacts
@@ -9,6 +11,11 @@ app = FastAPI()
 
 app.include_router(auth.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')
+
+@app.on_event('startup')
+async def startup():
+    r = Redis(host='localhost', port=6379, db=0, encoding='utf-8', decode_responses=True)
+    await FastAPILimiter.init(r)
 
 @app.get('/')
 def main():
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, Query, status
+from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -12,7 +13,8 @@ from src.services.auth import auth_service
 
 router = APIRouter(prefix='/contacts', tags=['contacts'])
 
-@router.get('/', response_model=List[ContactResponce])
+@router.get('/', response_model=List[ContactResponce],
+            dependencies=[Depends(RateLimiter(times=1, seconds=5))])
 async def get_contacts(skip: int,
                         limit: int,
                         current_user: User = Depends(auth_service.get_current_user),
